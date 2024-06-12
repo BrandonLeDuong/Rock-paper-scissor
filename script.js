dce53ca08d0e5130240ab99c0321b4b37fafd24b
Brandon Duong <bduong1497@gmail.com>
2024-06-11 21:44:22 -0400

    added reset button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 let round = 0;
-let timeLeft = 30;
+let timeLeft = 10;
 let countdown;
 
 const roundDisplay = document.getElementById("roundDisplay");
@@ -15,6 +15,7 @@ const computerHealth = document.getElementById("computerHealth");
 const rockButton = document.getElementById("rockBtn");
 const paperButton = document.getElementById("paperBtn");
 const scissorButton = document.getElementById("scissorBtn");
+const resetButton = document.getElementById("resetBtn");
 
 rockButton.addEventListener("click", function () {
   animateChoice(rockButton);
@@ -31,20 +32,42 @@ scissorButton.addEventListener("click", function () {
   playGame("scissor");
 });
 
+resetButton.addEventListener("click", function () {
+  clearInterval(countdown);
+  restartGame();
+});
+
 function updateTimer() {
-  timerDisplay.textContent = `Times left ${timeLeft} seconds`;
+  timerDisplay.textContent = `${timeLeft}`;
   if (timeLeft === 0) {
     clearInterval(countdown);
-    resultDisplay.textContent = `Time's up, you didn't make a choice!`;
+    timerDisplay.textContent = `Time's up, you didn't make a choice!`;
     disableButtons();
     selectRandomChoice();
+
+    return;
   }
+
   timeLeft--;
 }
 
 function startCountdown() {
   clearInterval(countdown);
+  timeLeft = 10;
+  updateTimer();
   countdown = setInterval(updateTimer, 1000);
+}
+
+function selectRandomChoice() {
+  clearInterval(countdown);
+  const randomChoice = getComputerChoice();
+  if (randomChoice === "rock") {
+    rockButton.click();
+  } else if (randomChoice === "paper") {
+    paperButton.click();
+  } else if (randomChoice === "scissor") {
+    scissorButton.click();
+  }
 }
 
 function animateChoice(button) {
@@ -63,7 +86,10 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
   startCountdown();
   round++;
-  roundDisplay.textContent = `Round: ${round}`;
+  enableButtons();
+  roundDisplay.textContent = `${round}`;
+
+  resultDisplay.classList.remove("result-win", "result-lose", "result-tie");
 
   const outcomes = {
     paper: { rock: "You win!", scissor: "You lose!" },
@@ -73,7 +99,7 @@ function playRound(humanChoice, computerChoice) {
 
   if (humanChoice === computerChoice) {
     resultDisplay.textContent = `It's a tie!`;
-    // console.log(`It's a tie!`);
+    resultDisplay.classList.add("result-tie");
   } else {
     const result = outcomes[humanChoice][computerChoice];
     resultDisplay.textContent = `${result} You picked ${humanChoice}, computer picked ${computerChoice}.`;
@@ -82,9 +108,11 @@ function playRound(humanChoice, computerChoice) {
     console.log(result);
 
     if (result === "You win!") {
+      resultDisplay.classList.add("result-win");
       humanScore++;
       playerDisplay.textContent = `Player Score: ${humanScore}`;
     } else if (result === "You lose!") {
+      resultDisplay.classList.add("result-lose");
       computerScore++;
       computerDisplay.textContent = `Computer Score: ${computerScore}`;
     }
@@ -95,8 +123,8 @@ function playRound(humanChoice, computerChoice) {
 
 function updateHealthScore() {
   const maxScore = 5;
-  const playerHealthPercent = ((maxScore - humanScore) / maxScore) * 100;
-  const computerHealthPercent = ((maxScore - computerScore) / maxScore) * 100;
+  const playerHealthPercent = (humanScore / maxScore) * 100;
+  const computerHealthPercent = (computerScore / maxScore) * 100;
   playerHealth.style.width = `${playerHealthPercent}%`;
   computerHealth.style.width = `${computerHealthPercent}%`;
 }
@@ -109,6 +137,7 @@ function playGame(humanSelection) {
 
   if (humanScore === 5 || computerScore === 5) {
     alert("Game Over");
+    clearInterval(countdown);
     restartGame();
   }
 }
@@ -117,16 +146,22 @@ function restartGame() {
   round = 0;
   computerScore = 0;
   humanScore = 0;
-  roundDisplay.textContent = `Round: ${round}`;
+
+  roundDisplay.textContent = `${round}`;
   playerDisplay.textContent = `Player Score: ${humanScore}`;
   computerDisplay.textContent = `Computer Score: ${computerScore}`;
   resultDisplay.textContent = `First to 5 wins!`;
-  disableButtons();
-  //   playerHealth.style.width = "100%";
-  //   computerHealth.style.width = "100%";
+  playerHealth.style.width = "0%";
+  computerHealth.style.width = "0%";
+  timerDisplay.textContent = 10;
+  enableButtons();
 }
 
 function disableButtons() {
   const buttons = document.querySelectorAll(".choice-image");
   buttons.forEach((button) => (button.style.pointerEvents = "none"));
+}
+function enableButtons() {
+  const buttons = document.querySelectorAll(".choice-image");
+  buttons.forEach((button) => (button.style.pointerEvents = "auto"));
 }
